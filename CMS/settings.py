771dbd57b81a8ff5244f475976c2eef9a9a7657b
Pragyanshu-rai <pragyanshur@gmail.com>
2021-06-26 22:48:52 +0530

    final modification to settings.py file

--- CMS/settings.py
+++ CMS/settings.py
@@ -140,10 +140,4 @@ MEDIA_ROOT = path.join(BASE_DIR, 'static/cms/media')
 
 
 # set session cookie default age
-<<<<<<< HEAD
-SESSION_COOKIE_AGE = 1*60*1000 #five minutes
-||||||| 5296925
-SESSION_COOKIE_AGE = 3*60*1000 #five minutes
-=======
 SESSION_COOKIE_AGE = 3*60 #five minutes
->>>>>>> dev
