a5e45e028e2985ec5e370bc0e645aa834a517422
Pragyanshu <pragyanshu@prakya.com>
2023-09-05 14:07:04 +0530

    changes applied for prod deployment

---
+++ buid.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'starting build sequence';
+sudo apt install -y python3-pip;
+pip install -r requirements.txt;
+python3 manage.py makemigrations;
+python3 manage.py migrate;
+echo 'build Complete';
+echo 'starting deployment sequence';
+python3 manage.py runserver;
+echo 'deployment complete';
